a68c2827e31473fc2431b1831ab41ca6fe6d3eaa
Yo1 <bigdiebam@gmail.com>
2023-05-02 02:06:25 +0300

    Check

--- polls/urls.py
+++ polls/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("teachers/", views.teacher_list, name="teachers_display"),
     path("group/", views.add_group, name="add_group"),
     path("groups/", views.group_list, name="group_display"),
-
 ]
